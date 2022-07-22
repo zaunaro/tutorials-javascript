e844999e767ad5d4059af8407b25a4eb34128486
Aquitano <44656811+Aquitano@users.noreply.github.com>
2022-07-22 10:38:44 +0200

    feat: Add solutions for twoSum & parseLocalization

--- solution/parse_localization.js
+++ solution/parse_localization.js
@@ -7,23 +7,33 @@
 //expected output
 //{ text: "hallo", i18n: { cz: { "text": "Ahoy"}, nl: { "text": "Hey"}} }
 
-let input = [{ de: "hallo" }, { cz: "Ahoj" }, { nl: "Hey" }];
+let aInput = [{ de: "hallo" }, { cz: "Ahoj" }, { nl: "Hey" }];
 
-console.log(parseLocalization(input));
+console.log(parseLocalization(aInput));
 
 /* -------------------------------------- */
 function parseLocalization(aSource) {
-  const result = { text: "", i18n: {} };
+  const oResult = { text: "", i18n: {} };
 
   for (const source of aSource) {
     if (Object.keys(source) == "de") {
-      result.text = Object.keys(source).toString();
+      oResult.text = Object.keys(source).toString();
     } else {
-      result.i18n[Object.keys(source)] = {
+      oResult.i18n[Object.keys(source)] = {
         text: source[Object.keys(source)],
       };
     }
   }
 
-  return result;
+  aSource.forEach(source => {
+    const sKey = Object.keys(source)[0];
+    const sText = source[sKey]
+    if (sKey == "de") {
+      oResult.text = sText;
+    } else {
+      oResult.i18n[sKey] = { text: source[sKey] };
+    }
+  });
+
+  return oResult;
 }
